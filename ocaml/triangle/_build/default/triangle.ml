4d9095433f6d9d509f2dfb389aba5fac19396277
Mikolaj Kapica <mikolajkapica@mail.com>
2023-08-14 01:28:19 +0200

    solve triangle

---
+++ ocaml/triangle/_build/default/triangle.ml
@@ -0,0 +1,17 @@
+let is_triangle a b c =
+    a > 0 
+    && b > 0 
+    && c > 0 
+    && a + b >= c 
+    && b + c >= a 
+    && a + c >= b
+
+let is_equilateral a b c =
+    is_triangle a b c && a = b && b = c
+
+let is_isosceles a b c =
+    is_triangle a b c && (a = b || b = c || c = a)
+
+let is_scalene a b c =
+    is_triangle a b c && (a <> b && b <> c && a <> c)
+
